3936863b647ab5abd8f5b137287b6d230d698823
Jiyeong Seok <jiyeong.seok@lge.com>
2022-07-12 17:44:47 +0900

    Add compare mode - Compare two FOSSLight Report yaml
    Signed-off-by: Jiyeong Seok <jiyeong.seok@lge.com>

--- src/fosslight_scanner/cli.py
+++ src/fosslight_scanner/cli.py
@@ -10,10 +10,10 @@ from fosslight_util.help import print_package_version
 
 def main():
     parser = ArgumentParser(description='FOSSLight Scanner', prog='fosslight_scanner', add_help=False)
-    parser.add_argument('mode', nargs='?', help='source| dependency| binary| reuse| all', default="all")
+    parser.add_argument('mode', nargs='?', help='source| dependency| binary| reuse| all| compare', default="all")
     parser.add_argument('--path', '-p', help='Path to analyze', type=str, dest='path', default="")
     parser.add_argument('--wget', '-w', help='Link to be analyzed', type=str, dest='link', default="")
-    parser.add_argument('--file', '-f', help='Output file format (excel, csv, opossum)', type=str, dest='file', default="")
+    parser.add_argument('--file', '-f', help='Output file format (excel, csv, opossum, yaml)', type=str, dest='file', default="")
     parser.add_argument('--output', '-o', help='Output directory or file', type=str, dest='output', default="")
     parser.add_argument('--dependency', '-d', help='Dependency arguments', type=str, dest='dep_argument', default="")
     parser.add_argument('--url', '-u', help="DB Url", type=str, dest='db_url', default="")
@@ -22,6 +22,7 @@ def main():
     parser.add_argument('--timer', '-t', help='Hide the progress bar',  action='store_true', dest='timer', default=False)
     parser.add_argument('--version', '-v', help='Print version',  action='store_true', dest='version', default=False)
     parser.add_argument('--help', '-h', help='Print help message', action='store_true', dest='help')
+    parser.add_argument('--yaml', '-y', help='Two FOSSLight reports in yaml format', nargs=2, default="")
     try:
         args = parser.parse_args()
     except SystemExit:
@@ -32,8 +33,15 @@ def main():
     elif args.version:
         print_package_version(PKG_NAME, "FOSSLight Scanner Version:")
     else:
+        if args.yaml:
+            before_yaml = args.yaml[0]
+            after_yaml = args.yaml[1]
+        else:
+            before_yaml = ''
+            after_yaml = ''
+
         run_main(args.mode, args.path, args.dep_argument, args.output, args.file,
-                 args.link, args.db_url, args.timer, args.raw, args.core)
+                 args.link, args.db_url, args.timer, args.raw, args.core, before_yaml, after_yaml)
 
 
 if __name__ == "__main__":
