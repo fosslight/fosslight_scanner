79835ec5f2f63590aa75105caa87b6785144c181
cjho0316 <cjho03160316@gmail.com>
2024-09-28 04:16:25 +0900

    fix: fix lint and field in help
    Signed-off-by: cjho0316 <cjho03160316@gmail.com>

--- tests/test__help.py
+++ tests/test__help.py
@@ -1,12 +1,12 @@
 import sys
-import pytest
 from fosslight_scanner._help import print_help_msg, _HELP_MESSAGE_SCANNER
+
 
 def test_print_help_msg(capsys, monkeypatch):
     # given
     # monkeypatch sys.exit to prevent the test from stopping
     monkeypatch.setattr(sys, "exit", lambda: None)
-    
+
     # when
     print_help_msg()
 
