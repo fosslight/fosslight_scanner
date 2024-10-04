8febb0e3e1c3e9a3af7c028601adf228df31d3b6
cjho0316 <cjho03160316@gmail.com>
2024-10-04 23:32:05 +0900

    fix: test__parse_setting
    Signed-off-by: cjho0316 <cjho03160316@gmail.com>

--- tests/test__parse_setting.py
+++ tests/test__parse_setting.py
@@ -1,43 +1,66 @@
-# import pytest
-# from fosslight_scanner._parse_setting import parse_setting_json
+from fosslight_scanner._parse_setting import parse_setting_json
 
 
-# @pytest.mark.parametrize("data, expected_output, expect_warning", [
-#     (
-#         {
-#             "mode": ["source", "binary"],
-#             "path": ["/path/to/scan"],
-#             "dep_argument": "--arg",
-#             "output": "output_directory",
-#             "format": "excel",
-#             "link": "https://example.com",
-#             "db_url": "postgresql://user:pass@host/db",
-#             "timer": True,
-#             "raw": True,
-#             "core": 4,
-#             "no_correction": True,
-#             "correct_fpath": "sbom-info.yaml",
-#             "ui": False,
-#             "exclude": ["excluded/path"]
-#         },
-#         (
-#             ["source", "binary"], ["/path/to/scan"], "--arg", "output_directory", "excel",
-#             "https://example.com", "postgresql://user:pass@host/db", True, True, 4, True,
-#             "sbom-info.yaml", False, ["excluded/path"]
-#         ),
-#         False
-#     ),
-# ])
-# def test_parse_setting_json(data, expected_output, expect_warning, capsys):
-#     # when
-#     result = parse_setting_json(data)
+def test_parse_setting_json_valid(capsys):
+    # given
+    data = {
+        "mode": ["source", "binary"],
+        "path": ["/path/to/scan"],
+        "dep_argument": "--arg",
+        "output": "output_directory",
+        "format": "excel",
+        "link": "https://example.com",
+        "db_url": "***host/db",
+        "timer": True,
+        "raw": True,
+        "core": 4,
+        "no_correction": True,
+        "correct_fpath": "sbom-info.yaml",
+        "ui": False,
+        "exclude": ["excluded/path"],
+        "selected_source_scanner": "scanner",
+        "source_write_json_file": True,
+        "source_print_matched_text": False,
+        "source_time_out": 300,
+        "binary_simple": True
+    }
+    expected_output = (
+        ["source", "binary"], ["/path/to/scan"], "--arg", "output_directory", "excel",
+        "https://example.com", "***host/db", True, True, 4, True, "sbom-info.yaml",
+        False, ["excluded/path"], "scanner", True, False, 300, True
+    )
 
-#     # then
-#     assert result == expected_output
+    # when
+    result = parse_setting_json(data)
 
-#     # Invalid data should produce a warning message
-#     captured = capsys.readouterr()
-#     if expect_warning:
-#         assert 'Ignoring some values with incorrect format in the setting file.' in captured.out
-#     else:
-#         assert 'Ignoring some values with incorrect format in the setting file.' not in captured.out
+    # then
+    assert result == expected_output
+
+    # Check no warning is printed
+    captured = capsys.readouterr()
+    assert 'Ignoring some values with incorrect format in the setting file.' not in captured.out
+
+
+def test_parse_setting_json_invalid(capsys):
+    # given
+    data = {
+        "mode": "source",  # Should be list
+        "path": "/path/to/scan",  # Should be list
+        "core": "not_an_int",  # Should be int
+        "timer": "not_a_bool",  # Should be bool
+        "source_time_out": "not_an_int",  # Should be int
+    }
+    expected_output = (
+        [], [], '', '', '', '', '', False, False, -1, False, '',
+        False, [], '', False, False, 120, False
+    )
+
+    # when
+    result = parse_setting_json(data)
+
+    # then
+    assert result == expected_output
+
+    # Check warning is printed
+    captured = capsys.readouterr()
+    assert 'Ignoring some values with incorrect format in the setting file.' in captured.out
