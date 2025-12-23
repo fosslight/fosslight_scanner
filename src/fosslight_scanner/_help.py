6e01b26e977ae0d4e50109c06393041e6f2ec7f4
석지영/책임연구원/SW공학(연)Open Source TP <jiyeong.seok@lge.com>
2025-12-23 18:26:06 +0900

    Update supported format
    Signed-off-by: 석지영/책임연구원/SW공학(연)Open Source TP <jiyeong.seok@lge.com>

--- src/fosslight_scanner/_help.py
+++ src/fosslight_scanner/_help.py
@@ -3,8 +3,9 @@
 # Copyright (c) 2021 LG Electronics Inc.
 # SPDX-License-Identifier: Apache-2.0
 from fosslight_util.help import PrintHelpMsg
+from fosslight_util.output_format import SUPPORT_FORMAT
 
-_HELP_MESSAGE_SCANNER = """
+_HELP_MESSAGE_SCANNER = f"""
     FOSSLight Scanner performs open source analysis after downloading the source from URL that can be cloned by git or wget.
     Instead, open source analysis and checking copyright/license rules can be performed for the local source path.
     The output result is generated in OSS Report format.
@@ -21,14 +22,14 @@ _HELP_MESSAGE_SCANNER = """
 
         Options:
             -h\t\t\t    Print help message
-            -p <path>\t\t    Path to analyze (ex, -p {input_path})
+            -p <path>\t\t    Path to analyze (ex, -p [input_path])
                                      * Compare mode input file: Two FOSSLight reports (supports excel, yaml)
-                                       (ex, -p {before_name}.xlsx {after_name}.xlsx)
+                                       (ex, -p [before_name].xlsx [after_name].xlsx)
             -w <link>\t\t    Link to be analyzed can be downloaded by wget or git clone
-            -f <formats> [<format> ...]\t    FOSSLight Report file format (excel, csv, opossum, yaml, spdx-tag, spdx-yaml, spdx-json, spdx-xml)
+            -f <formats> [<format> ...]\t    FOSSLight Report file format ({', '.join(SUPPORT_FORMAT)})
                                      * Compare mode result file: supports excel, json, yaml, html
                                      * Multiple formats can be specified separated by space.
-            -e <path>\t\t    Path to exclude from analysis (ex, -e {dir} {file})
+            -e <path>\t\t    Path to exclude from analysis (ex, -e [dir] [file])
             -o <output>\t\t    Output directory or file
             -c <number>\t\t    Number of processes to analyze source
             -r\t\t\t    Keep raw data
